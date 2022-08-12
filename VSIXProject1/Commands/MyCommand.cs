7e3d4564e382a4b4adc1dfb7d1d6b9cf29114f66
Ali İhsan AKAY <ali.akay@obase.com>
2022-08-12 15:20:50 +0300

    Add project files.

---
+++ VSIXProject1/Commands/MyCommand.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace VSIXProject1
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            
+            
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+            if (selection.HasValue)
+            {
+                var newText = GenerateNewClass(selection.Value.GetText());
+                if (newText != null)
+                {
+                    var concatText = $"{selection.Value.GetText()} \n {newText}";
+                    docView.TextBuffer.Replace(selection.Value, concatText);
+                }
+            }
+        }
+        private string GenerateNewClass(string text)
+        {
+            StringBuilder newClass = new StringBuilder();
+
+            try
+            {
+                var splitText = text.Split('\n');
+
+                foreach (var item in splitText)
+                {
+                    if (!string.IsNullOrWhiteSpace(item))
+                    {
+                        var prop = Regex.Replace(item.Trim(), @"\s+", "");
+                        var clearGetSet = prop.Replace("{get;set;}", "").Replace("DateTime", "datetime");
+                        var lasSpace = clearGetSet.IndexOfAny(UpperCaseChars);
+                        var propName = clearGetSet.Substring(lasSpace, clearGetSet.Length - lasSpace);
+                        var propType = clearGetSet.Substring(0, lasSpace).Replace("public", "");
+
+                        var yeniProp = "";
+                        var isFirst = true;
+                        foreach (var propItem in propName)
+                        {
+                            if (isFirst)
+                                yeniProp += propItem;
+                            else
+                            {
+                                if (propItem == '_')
+                                {
+                                    isFirst = true;
+                                    continue;
+                                }
+                                else
+                                {
+                                    yeniProp += propItem.ToString().ToLower(new CultureInfo("en-EN", false));
+                                }
+                            }
+
+                            isFirst = false;
+                        }
+                        propType = propType.Replace("datetime", "DateTime").Replace("datetime?", "DateTime?");
+                        var yeniSnakeCaseProp = "public " + propType + " " + yeniProp + " {get;set;}";
+                        newClass.AppendLine(yeniSnakeCaseProp);
+
+
+                    }
+                }
+                return newClass.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                VS.MessageBox.ShowError("Hay Aksi !!!", "Sadece Class içini kopyalamalısın \n"+ex.Message);
+                return null;
+            }
+            
+
+        }
+        char[] UpperCaseChars = new char[]
+           {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                'Y', 'Z'
+           };
+    }
+}
